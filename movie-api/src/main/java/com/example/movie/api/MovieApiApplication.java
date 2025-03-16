aa7315b39bec1877c95b1e5bc929c38e482cf76d
poow810 <poow810@naver.com>
2025-03-16 18:59:05 +0900

    fix: 의존성 주입 문제 해결

--- movie-api/src/main/java/com/example/movie/api/MovieApiApplication.java
+++ movie-api/src/main/java/com/example/movie/api/MovieApiApplication.java
@@ -2,8 +2,12 @@ package com.example.movie.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.movie"})
+@EnableJpaRepositories(basePackages = "com.example.movie.domain.repository")
+@EntityScan(basePackages = "com.example.movie.domain.entity")
 public class MovieApiApplication {
 
     public static void main(String[] args) {
