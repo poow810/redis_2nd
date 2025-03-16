72f6aca84353cc19ad3122e6b3d2057db5011cf7
poow810 <poow810@naver.com>
2025-03-16 18:38:46 +0900

    feat: 로그 추가

--- movie-api/src/main/java/com/example/movie/api/controller/MovieController.java
+++ movie-api/src/main/java/com/example/movie/api/controller/MovieController.java
@@ -3,8 +3,10 @@ package com.example.movie.api.controller;
 
 import com.example.movie.domain.dto.MovieResponseDto;
 import com.example.movie.domain.service.MovieService;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movies")
 @RequiredArgsConstructor
+@Slf4j
 public class MovieController {
 
     private final MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<MovieResponseDto>> getAllMovies() {
+    public ResponseEntity<List<MovieResponseDto>> getAllMovies(HttpServletRequest request) {
+        log.info(String.valueOf(request));
         List<MovieResponseDto> movies = movieService.getMovies();
         return ResponseEntity.status(HttpStatus.OK).body(movies);
     }
