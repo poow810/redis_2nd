3916d1bc758487596d391977b31e3623c8144224
poow810 <poow810@naver.com>
2025-03-17 01:30:03 +0900

    fix : componantscan 추가

--- movie-api/src/main/java/com/example/movie/api/MovieApiApplication.java
+++ movie-api/src/main/java/com/example/movie/api/MovieApiApplication.java
@@ -3,9 +3,11 @@ package com.example.movie.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.example.movie"})
+@SpringBootApplication
+@ComponentScan(basePackages = "com.example.movie")
 @EnableJpaRepositories(basePackages = "com.example.movie.domain.repository")
 @EntityScan(basePackages = "com.example.movie.domain.entity")
 public class MovieApiApplication {
